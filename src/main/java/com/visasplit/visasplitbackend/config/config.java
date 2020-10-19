9ed83f96455e2a48917468f2b4c502ceb9a1f838
Singh <rajat789pratap@gmail.com>
2020-10-19 13:05:46 +0530

    Config file added

---
+++ src/main/java/com/visasplit/visasplitbackend/config/config.java
@@ -0,0 +1,8 @@
+package com.visasplit.visasplitbackend.config;
+
+import lombok.Getter;
+
+@Getter
+public class config {
+    private static String mongoUrl="mongodb+srv://VisaUser:VisaPass@cluster0.7ox8y.mongodb.net/test";
+}
