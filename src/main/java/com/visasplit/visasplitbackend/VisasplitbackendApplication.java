ba532a3ce7b35da7763aa19b3245f5fcd28711af
Singh <rajat789pratap@gmail.com>
2020-10-19 12:00:45 +0530

    init commit:made project skeleton  w/o security

---
+++ src/main/java/com/visasplit/visasplitbackend/VisasplitbackendApplication.java
@@ -0,0 +1,13 @@
+package com.visasplit.visasplitbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisasplitbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VisasplitbackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/visasplit/visasplitbackend/VisasplitbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.visasplit.visasplitbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisasplitbackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
